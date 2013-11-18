7a97d19d94590a28e2862620c2a1840c9fcc9c14
Shun Zhang <jensen.zhang@utexas.edu>
2013-11-18 17:49:47 -0600

    add compare.m for debug

---
+++ src/compare.m
@@ -0,0 +1,11 @@
+args = argv();
+disp(args);
+x = 0 : 500;
+y = args{1} * (x .^ 2) + args{2} * x + args{3};
+plot (x, y, "r");
+hold on;
+
+d = dlmread("obs.data");
+plot (d(:, 2), d(:, 1));
+
+pause;
