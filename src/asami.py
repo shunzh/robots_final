317309f21eb5b73fb2f17b8b2612898e23f4a910
Shun Zhang <jensen.zhang@utexas.edu>
2013-11-19 16:16:59 -0600

    sensor to action also okay

--- src/asami.py
+++ src/asami.py
@@ -7,18 +7,20 @@ import copy
 import simulate
 
 class Asami:
-  T_START = 50
+  T_START = 5
   Lambda = .1
 
   def __init__(self):
-    self.t = 0
+    self.t = 50
     self.w_a = 0
     self.w_s = 0
 
     # action model with init config
-    self.actionModel = ActionModel(np.array([0, .1, 0]))
+    #self.actionModel = ActionModel(np.array([0, .1, 0]))
+    self.actionModel = ActionModel(np.array([0, 0.001, 0]))
     self.actionModelInit = self.actionModel.copy()
 
+    #self.sensorModel = SensorModel(np.array([-1.9135e-01, 4.0880e+01, -1.7393e+03]))
     self.sensorModel = SensorModel(np.array([0, 0, 0]))
 
     self.world = simulate.Simulator()
@@ -36,7 +38,7 @@ class Asami:
     if self.t > self.T_START:
       old_w_s = self.w_s
       self.w_s = self.sensorModel[obs]
-      #self.actionModel.update(action, self.w_s - old_w_s)
+      self.actionModel.update(action, self.w_s - old_w_s)
 
       self.w_a = (1 - self.Lambda) * self.w_a + self.Lambda * self.w_s
 
@@ -79,7 +81,7 @@ SensorModel = PolyRegressionModel
 # unit test
 if __name__ == "__main__":
   a = Asami()
-  for i in range(200):
+  for i in range(500):
     a.run()
     print "Iteration #", i
     print "w_a", a.w_a
